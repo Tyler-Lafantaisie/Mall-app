79483da88025c56f1f13b07a87c12c53410f09cd
Tyler Lafantaisie <tylerlafantaisie@gmail.com>
2023-04-26 12:10:20 -0400

    rewriting code

--- src/header.tsx
+++ src/header.tsx
@@ -1,31 +1,108 @@
-import "react";
-import cart from "./assets/img/Shopping cart.svg";
+import {useState} from "react";
+import {ReactComponet as cart}from "./assets/img/Shopping cart.svg";
 import hamburger from "./assets/img/hambuger.svg";
-function Header(){
-  
+
+ export default function Header(){
+   return( 
+    <header className="bg-slate-700 grid grid-cols-3 grid-rows-1 w-full">
+ 
+    <Navbar>
+        <NavItem icon = {hamburger} >
+            <DropdownMenu/> 
+        </NavItem>
+    </Navbar>
+
+    </header>
+);
+}
+
+function DropdownMenu(){
+   function DropdownItem(props){
+   return(
+    <a className="">
+         
+        {props.children}
+
+    </a>
+   )
+   } 
+    return(
+        <div className="absolute top-5 bg-red-500 w-20 h-20">
+        <DropdownItem> Home </DropdownItem>
+        </div>
+    )
+}
+function Navbar(props){
+    return(
+         <nav>
+             <ul className="m-2">{props.children}</ul>
+        </nav>
+)
+}
+function NavItem(props){
+    const [open, setOpen] = useState(false);
+    return(
+        <li className="flex m-2 p-2">
+            <a className="" onClick={()=> setOpen(!open)}>
+                <img src={props.icon} className="w-14 sm:w-8"/> 
+            </a>
+
+            {open && props.children}
+        </li>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+    // change state 
+    /*
     function Ham_menu(){
+        
         const hamIcon = (  
               <div className="flex m-2  p-2">
-                <img src = {hamburger} alt="Hamberger menu" className="w-14 sm:w-8 " />
-                {/* div to open and close*/}
+                <img src = {hamburger} alt="Hamberger menu" className="w-14 sm:w-8 "/>
+                
+                <div className="border-2 bg-slate-700 border-slate-900 rounded-xl absolute z-10 w-48 h-72 open-close">
+                    <nav>
+                        <ul className="m-2">
+                        <img src = {hamburger} alt="Hamberger menu" className="w-14 sm:w-14" />
+
+                            <li className="text-slate-50 font-Domine text-lg my-1">My Home</li>
+                            <li className="text-slate-50 font-Domine text-lg my-1">About</li>
+                            <li className="text-slate-50 font-Domine text-lg my-1">Support</li>
+                            <li className="text-slate-50 font-Domine text-lg my-1">Wishlists</li>
+                            <li className="text-slate-50 font-Domine text-lg my-1">Settings</li>
+                            <li className="text-slate-50 font-Domine text-base my-1 mx-3">Payemnt Settings</li>
+                            <li className="text-slate-50 font-Domine text-base my-1 mx-3">Country</li>
+                            <li className="text-slate-50 font-Domine text-base my-1 mx-3">Age filter</li>
+                        </ul>
+                    </nav>
+                </div>
             </div>);
+            
         return(hamIcon);
 
-    };
-
+    }
+    // links to home page
     function Main_Title(){
 
-        const Title_name :string ="Mall app";
+        const Title_name ="Mall app";
         const Title_contents = (
             <div className="flex justify-center m-2 p-2">
             <h1 className="text-slate-50 font-Domine text-2xl sm:text-lg md:text-xl">{Title_name}</h1>
         </div>) 
-        
-        return(Title_contents)
-    };
-
+        return (Title_contents)
+    }
+    // changes state when someone adds something to the cart
     function Shopping_Cart(){
-        let cart_items :number = 0;
+        const cart_items = 0;
         const Indicator =(
             <div className="w-10 h-10 absolute flex justify-center items-center indicator">
                     <div className="bg-red-500 w-5 h-5 rounded-full flex justify-center">
@@ -57,7 +134,6 @@ function Header(){
                 <Shopping_Cart/>
             </header>
         </>
-    );
-}
+    );*/
 
-export default Header;
+
